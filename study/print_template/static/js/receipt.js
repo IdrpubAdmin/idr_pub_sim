097ea1999964df4f47627eee4f082e26451ca010
seop2777 <seop277@idrsystem.co.kr>
2022-01-21 17:58:31 +0900

    영수증 페이지 수정

--- study/print_template/static/js/receipt.js
+++ study/print_template/static/js/receipt.js
@@ -1,15 +1,13 @@
 const taxOpen = document.querySelector('.tax-btn');
 const cashOpen = document.querySelector('.cash-btn');
-
 const taxCnts = document.querySelector('.tax');
 const cashCnts = document.querySelector('.cash');
-
 const closeBtn = document.querySelectorAll('.close');
 
 closeBtn.forEach((targetClose) => {
-    let cntsClose = document.querySelectorAll('.cnts-wrap');
     targetClose.addEventListener('click', () => {
-        cntsClose.classList.remove('active');
+        taxCnts.classList.remove('active');
+        cashCnts.classList.remove('active');
     });
 });
 
