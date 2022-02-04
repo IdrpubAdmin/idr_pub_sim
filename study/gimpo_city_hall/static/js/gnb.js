4fc80ceda501be821de22dcac437707264a15324
seop2777 <seop277@idrsystem.co.kr>
2022-02-04 17:54:37 +0900

    gnb작업

--- study/gimpo_city_hall/static/js/gnb.js
+++ study/gimpo_city_hall/static/js/gnb.js
@@ -10,3 +10,30 @@ for(let i = 0; i < dropDown.length; i++){
       hideMenu.classList.remove('active');
    });
 }
+
+const mOpen = document.querySelector('.m-open');
+const mClose = document.querySelector('.m-close');
+const mWrap = document.querySelector('.mnb-wrap');
+const mSlide = document.querySelector('.mnb');
+
+mOpen.addEventListener('click', function(){
+   mWrap.classList.add('m-active');
+   mSlide.classList.add('m-slide');
+});
+
+mClose.addEventListener('click', function(){
+   mWrap.classList.remove('m-active');
+   mSlide.classList.remove('m-slide');
+});
+
+const mBtn = document.querySelectorAll('.drop');
+const mDropDown = document.querySelectorAll('.mnb-item');
+
+for(let j = 0; j < mBtn.length; j++){
+   mBtn[j].addEventListener('click', function(e){
+      for(let x = 0; x < mDropDown.length; x++){
+         mDropDown[x].parentNode.classList.remove('active');
+         e.target.classList.add('active');
+      }
+   });
+}
