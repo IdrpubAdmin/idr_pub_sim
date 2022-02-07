46de08099b88106ad961959fdd96596b66392650
seop2777 <seop277@idrsystem.co.kr>
2022-02-07 18:01:47 +0900

    GNB 모바일메뉴

--- study/gimpo_city_hall/static/js/gnb.js
+++ study/gimpo_city_hall/static/js/gnb.js
@@ -1,39 +1,74 @@
+// GNB 메뉴
+
 const dropDown = document.querySelectorAll('.lnb-item');
 
-for(let i = 0; i < dropDown.length; i++){
-    dropDown[i].addEventListener('mouseover', function(){
+for (let i = 0; i < dropDown.length; i++) {
+   dropDown[i].addEventListener('mouseover', function () {
       showMenu = this.querySelector('.snb-wrap');
       showMenu.classList.add('active');
    });
-   dropDown[i].addEventListener('mouseout', function(){
+   dropDown[i].addEventListener('mouseout', function () {
       hideMenu = this.querySelector('.snb-wrap');
       hideMenu.classList.remove('active');
    });
 }
+
+// 모바일 햄버거버튼
 
 const mOpen = document.querySelector('.m-open');
 const mClose = document.querySelector('.m-close');
 const mWrap = document.querySelector('.mnb-wrap');
 const mSlide = document.querySelector('.mnb');
 
-mOpen.addEventListener('click', function(){
+mOpen.addEventListener('click', function () {
    mWrap.classList.add('m-active');
    mSlide.classList.add('m-slide');
 });
 
-mClose.addEventListener('click', function(){
+mClose.addEventListener('click', function () {
    mWrap.classList.remove('m-active');
    mSlide.classList.remove('m-slide');
 });
 
+// 모바일 아코디언 
+
 const mBtn = document.querySelectorAll('.drop');
 const mDropDown = document.querySelectorAll('.mnb-item');
 
-for(let i = 0; i < mBtn.length; i++){
-   mBtn[i].addEventListener('click', function(e){
-      for(let x = 0; x < mDropDown.length; x++){
-         mDropDown[x].parentNode.classList.remove('active');
-         e.target.classList.add('active');
+for (let i = 0; i < mBtn.length; i++) {
+   mBtn[i].addEventListener('click', function (e) {
+      for (let x = 0; x < mDropDown.length; x++) {
+         if(getComputedStyle(e.target).height == '50px'){
+            mDropDown[x].parentNode.classList.remove('active');
+            e.target.classList.add('active');     
+         } else{
+            e.target.classList.remove('active');   
+         }
       }
    });
 }
+
+// 팝업닫기
+
+const popupBtn = document.querySelector('.popup-close');
+const popupClose = document.querySelector('.popup-wrap');
+
+popupBtn.addEventListener('click', function(){
+   popupClose.classList.add('active');
+});
+
+
+   // 날짜 추출 (반환) date.getDate();
+   // 날짜 지정 date.setDate(새로운 날짜);
+   var date = new Date();
+   date.setDate(date.getDate() + 7);
+
+ 
+   var setCookie = '';
+   // setCookie = setCookie + 'CookieName=ABC;';
+   setCookie += 'CookieName=ABC;';
+   setCookie += 'Expires=' + date.toUTCString();
+
+   document.cookie = setCookie; //쿠키 설정, 생성
+
+   console.log(document.cookie);
