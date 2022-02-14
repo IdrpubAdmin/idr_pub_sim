863e27ce6734f8f4e16a5e1cb8c3507f751857d9
seop2777 <seop277@idrsystem.co.kr>
2022-02-14 17:57:03 +0900

    vue 실습

--- study/vuejs_guide/cli/5.가위바위보/webpack.config.js
+++ study/vuejs_guide/cli/5.가위바위보/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     module: {
         rules: [{
             test: /\.vue$/,
-            use: 'vue-loader',
+            loader: 'vue-loader',
         }, {
             test: /\.css$/,
             use: [
