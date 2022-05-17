e418db901c428a890fe86c94b0ef3cc15681e833
seop2777 <seop277@idrsystem.co.kr>
2022-05-17 17:46:14 +0900

    프로필

--- study/react_guide/twitter/twitter/src/components/Router.js
+++ study/react_guide/twitter/twitter/src/components/Router.js
@@ -8,7 +8,7 @@ import Navigation from "components/Navigation";
 const AppRouter = ({isLoggedIn, userObj}) => {
     return (
     <HashRouter>
-        {isLoggedIn && <Navigation />}
+        {isLoggedIn && <Navigation userObj={userObj} />}
         <div style={{ maxWidth: 890, width: "100%", margin: "0 auto", marginTop: 80, display: "flex", justifyContent: "center", }}>
             <Routes>
                 { isLoggedIn ? 
--- study/react_guide/twitter/twitter/src/routes/Profile.js
+++ study/react_guide/twitter/twitter/src/routes/Profile.js
@@ -1,10 +1,12 @@
 import { authService, dbService } from "fbase";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { collection, getDocs, query, where } from "firebase/firestore";
+import { updateProfile } from "@firebase/auth";
 
 const Profile = ({ userObj }) => {
     const navigate = useNavigate();
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName)
     const onLogOutClick = () => {
         authService.signOut();
         navigate("/");
@@ -18,14 +20,31 @@ const Profile = ({ userObj }) => {
         querySnapshot.forEach((doc) => {
             console.log(doc.id, " => ", doc.data());
         });
+    };
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setNewDisplayName(value);
+    };
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        if (userObj.displayName !== newDisplayName) {
+            await updateProfile(userObj, { displayName: newDisplayName });
+        }
     }
     useEffect(() => {
         getMyTweets();
     }, [])
     return(
         <>
-            <button onClick={onLogOutClick}>Log Out</button>
-            <span>Profile</span>
+            <div className="container">
+                <form onSubmit={onSubmit} className="profileForm">
+                    <input onChange={onChange} type="text" placeholder="Display name" value={newDisplayName} className="formInput" />
+                    <input type="submit" placeholder="Update Profile" className="formBtn" style={{marginTop: 10}} />
+                </form>
+                <span className="formBtn cancelBtn logOut" onClick={onLogOutClick}>Log Out</span>
+            </div>
         </>
     );
 };
