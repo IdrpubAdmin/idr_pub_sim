987601a32db78c6ce6a12d3eee28a5e81c893cc0
seop2777 <ryush@idrsystems.co.kr>
2022-07-18 17:52:34 +0900

    git test

---
+++ study/megabox/main.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import Megabox from './pages/Megabox';
+
+new Vue({
+  render: createElement => createElement(Megabox)
+}).$mount('#root');
---
+++ study/megabox/pages/Megabox.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        메가박스입니다아아아아아아아아
+    </div>
+</template>
+
+<script>
+
+export default {
+    components: { 
+
+    },
+
+    methods: {
+    
+    }
+};
+</script>
