dc5695fb3b4756ff7fd1fbaea11d0b357b5742e0
seop2777 <seop277@idrsystems.co.kr>
2022-08-10 09:16:37 +0900

    Chapter 6 : 트랜지션과 애니메이션

---
+++ study/vuejs_guide/vuestudy/my-app/src/store.js
@@ -0,0 +1,21 @@
+import Vue from "vue";
+import { createStore } from "vuex";
+
+Vue.use(Vuex);
+
+export default createStore({
+    state: {
+      counter: 10
+    },
+    getters: {
+      time2(state) {
+        return state.counter * 2;
+      }
+    },
+    mutations: {
+      setCounter(state, value) {
+        state.counter = value;
+      }
+    }
+  });
+   
