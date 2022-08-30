08f5b1f71bee06a3b5e23358f9115b9665520778
seop2777 <seop277@idrsystems.co.kr>
2022-08-30 17:59:53 +0900

    vuex actions

---
+++ study/vuejs_guide/vuestudy/고양이도할수있는Vuejs/vuex/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    {{ message }}
+    <div class="edit-form">
+      <input type="text" :value="message" @input="doUpdate">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  computed: {
+    message() {
+      return this.$store.getters.message
+    }
+  },
+  methods: {
+    doUpdate(e){
+      this.$store.dispatch('doUpdate', e.target.value)
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
