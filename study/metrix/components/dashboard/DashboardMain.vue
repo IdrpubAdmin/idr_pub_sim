e3f1621f1395881a0ba114dd55bcf24ea8f5de0b
seop2777 <seop277@idrsystems.co.kr>
2022-09-19 13:49:34 +0900

    중간저장

--- study/metrix/components/dashboard/DashboardMain.vue
+++ study/metrix/components/dashboard/DashboardMain.vue
@@ -1,6 +1,29 @@
 <template>
-    <div>
-        Dashboard
+    <div class="dashboard">
+        <section class="cnt1">
+            <article>컨1</article>
+            <article>컨2</article>
+            <article>컨3</article>
+        </section>
+        <section class="cnt2">
+            <div class="chart">
+                <div class="cnt3">
+                    <article>그래프</article>
+                    <div>
+                        <article>
+                            컨4
+                        </article>
+                        <article>
+                            컨5
+                        </article>
+                    </div>
+                </div>
+                <article class="graph">막대그래프</article>
+            </div>
+            <article class="prod">
+                상품
+            </article>
+        </section>
     </div>
 </template>
 
