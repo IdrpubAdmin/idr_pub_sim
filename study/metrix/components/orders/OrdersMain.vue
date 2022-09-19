3e34c97d843c262b1cb41c409a5e48a8c2c28afd
seop2777 <seop277@idrsystems.co.kr>
2022-09-19 13:59:28 +0900

    중간저장2

--- study/metrix/components/orders/OrdersMain.vue
+++ study/metrix/components/orders/OrdersMain.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         orders
-        <!-- <router-link :to="{name:'test1'}">1로이동</router-link>
-        <router-link :to="{name:'test2'}">2로이동</router-link>
-        <router-view></router-view> -->
+        <p><router-link :to="{name:'test1'}">1로이동</router-link></p>
+        <p><router-link :to="{name:'test2'}">2로이동</router-link></p>
+        <router-view></router-view>
     </div>
 </template>
 
