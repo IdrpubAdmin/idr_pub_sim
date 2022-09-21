604bb29ae0e48a370a4f00b79b7774e19bbacc67
seop2777 <seop277@idrsystems.co.kr>
2022-09-21 17:58:28 +0900

    settings 마크업

--- study/metrix/components/settings/SettingsMain.vue
+++ study/metrix/components/settings/SettingsMain.vue
@@ -1,6 +1,72 @@
 <template>
-    <div>
-        settings
+    <div class="settings">
+        <section>
+            <div class="nav">
+                <ul>
+                    <li>Account</li>
+                    <li>Business</li>
+                    <li>Security</li>
+                </ul>
+            </div>
+            <article>
+                <div class="settings-header">
+                    <h5>Account Settings</h5>
+                    <div class="btn">
+                        <button>
+                            Update
+                        </button>
+                    </div>           
+                </div>
+                <div class="profile">
+                    <ul>
+                        <li>
+                            <p>First Name</p>
+                            <input type="text">
+                            <img src="" alt="">
+                        </li>
+                        <li>
+                            <p>Last Name</p>
+                            <input type="text">
+                        </li>
+                        <li>
+                            <p>Email</p>
+                            <input type="text">
+                        </li>
+                        <li>
+                            <p>Phone Number</p>
+                            <input type="text">
+                        </li>
+                        <li>
+                            <p>Address</p>
+                            <input type="text">
+                        </li>
+                        <li>
+                            <p>City</p>
+                            <input type="text">
+                        </li>
+                        <li>
+                            <p>Country</p>
+                            <input type="text">
+                        </li>
+                        <li>
+                            <p>State</p>
+                            <input type="text">
+                        </li>
+                    </ul>
+                </div>
+                <div class="image">
+                    <img src="" alt="">
+                    <div class="btns">
+                        <button>
+                            <img src="" alt="">
+                        </button>
+                        <button>
+                            <img src="" alt="">
+                        </button>
+                    </div>
+                </div>
+            </article>
+        </section>
     </div>
 </template>
 
