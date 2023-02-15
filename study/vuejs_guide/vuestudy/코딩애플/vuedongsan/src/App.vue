2cec4862ec5fb5a8da7ae5208eeeb3f249f1c847
seop277 <seop277@idrsystems.co.kr>
2023-02-15 18:02:47 +0900

    코딩애플 뷰동산

--- study/vuejs_guide/vuestudy/코딩애플/vuedongsan/src/App.vue
+++ study/vuejs_guide/vuestudy/코딩애플/vuedongsan/src/App.vue
@@ -1,37 +1,86 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <div>
-    <h4>XX 원룸</h4>
-    <p>{{price1}} 만원</p>
-  </div>
-  <div>
-    <h4>XX 원룸</h4>
-    <p>{{price2}} 만원</p>
+    <div class="black-bg" v-if="모달창열렸니">
+      <div class="white-bg">
+        <img :src="원룸들[누른거].image" style="width:100%">
+        <h4>{{원룸들[누른거].title}}</h4>
+        <p>{{원룸들[누른거].content}}</p>
+        <p>{{원룸들[누른거].price}}</p>
+        <button @click="모달창열렸니 = false">닫기</button>
+      </div>
+    </div>
+    <div class="menu">
+      <a v-for="작명 in 메뉴들" :key="작명" href="">{{작명}}</a>
+    </div>
+    <Discount></Discount>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <div v-for="(원룸들,i) in 원룸들" :key="원룸들.id">
+      <img :src="원룸들.image" class="romm-img">
+      <h4 @click="모달창열렸니 = true; 누른거 = i">{{원룸들.title}}</h4>
+      <p>{{원룸들.price}}원</p>
+      <button @click="신고수[i]++">허위매물신고</button> <span>신고수 : {{신고수[i]}}</span>
+    </div>
   </div>
 </template>
 
 <script>
 
+import data from './assets/oneroom.js'
+import Discount from './DiscountComponent.vue'
+
 export default {
   name: 'App',
   data() {
     return {
-      price1: 60,
-      price2: 70,
+      누른거: 0,
+      원룸들 : data,
+      모달창열렸니 : false,
+      신고수: [0,0,0,0,0,0],
+      메뉴들: ['Home','Shop','Abut'],
     }
   },
   components: {
+    Discount : Discount
   }
 }
 </script>
 
 <style>
+body {
+  margin : 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height:100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+} 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+}
+.romm-img{
+  width: 100%;
+  margin-top: 40px;
+}
+
 </style>
