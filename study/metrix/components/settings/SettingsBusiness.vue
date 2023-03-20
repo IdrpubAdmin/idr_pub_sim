3aba3d4b90a3c58e450eef6ccce4e439a2efaebf
seop277 <97004955+seop2777@users.noreply.github.com>
2023-03-20 13:57:37 +0900

    js > json 변경

--- study/metrix/components/settings/SettingsBusiness.vue
+++ study/metrix/components/settings/SettingsBusiness.vue
@@ -1,18 +1,24 @@
 <template>
     <article>
         <div class="settings-header">
-            <button v-on:click="fetchData">get data</button>   
+            <button v-on:click="fetchData">get data</button>
+            <h1>{{id}}</h1>
         </div>
     </article>
 </template>
 
 <script>
 module.exports = {
+    data() {
+        return {
+            id : ''
+        }
+    },
     methods: {
     fetchData: function() {
-      axios.get('https://idrpubadmin.github.io/trial_pub_sim/study/metrix/assets/js/test_data.js')
+      axios.get('https://raw.githubusercontent.com/IdrpubAdmin/trial_pub_sim/main/study/metrix/assets/js/test_data.json')
         .then(function(response) {
-          console.log(response);
+            console.log(response.data);
         })
         .catch(function(error) {
           console.log(error);
