36cf9f9e30708f911e89606cc64b48df9b1989ff
seop277 <97004955+seop2777@users.noreply.github.com>
2023-03-24 10:36:32 +0900

    오류수정

--- study/cyclemon/assets/js/script.js
+++ study/cyclemon/assets/js/script.js
@@ -12,23 +12,12 @@ document.addEventListener('scroll', function(){
     }
 })
 
-let beforeScroll = window.pageYOffset
-
 document.addEventListener('scroll', function(){
     const navButton = document.querySelectorAll('nav ul li')
     const contents = document.querySelectorAll('.container section')
-    let afterScroll = window.pageYOffset
-    console.log(`전: ${beforeScroll}`)
-    console.log(`후: ${afterScroll}`)
-    let scroll += Number(afterScroll) - Number(beforeScroll)
-    let test = 0
-    test += scroll
-    console.log(test)
     for (let i = 0; i < navButton.length; i++) {
         if(navButton[i].classList.contains('active') && i < navButton.length - 1){
-            // console.log(contents[i].offsetHeight / 100)
-           contents[i].setAttribute('style', `background-position: 50% ${(contents[i].offsetHeight / 2) - scroll}px;`) // 48% 40% 30% ...
-           beforeScroll = afterScroll
+            // 시차이벤트
         }
     }
 })
