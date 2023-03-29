ff044c0facd7d677edd9127fbbd8f9c74aee0ae5
seop277 <97004955+seop2777@users.noreply.github.com>
2023-03-29 15:19:11 +0900

    html css정리 및 시차이벤트 추가

--- study/cyclemon/assets/js/script.js
+++ study/cyclemon/assets/js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('scroll', function(){
-    const navButton = document.querySelectorAll('nav ul li')
+    const navButton = document.querySelectorAll('.main-navigator ul li')
     const contents = document.querySelectorAll('.container section')
     const scroll = window.pageYOffset
     for (let i = 0; i < navButton.length; i++) {
@@ -12,18 +12,13 @@ document.addEventListener('scroll', function(){
     }
 })
 
-document.addEventListener('scroll', function(){
-    const navButton = document.querySelectorAll('nav ul li')
-    const contents = document.querySelectorAll('.container section')
-    for (let i = 0; i < navButton.length; i++) {
-        if(navButton[i].classList.contains('active') && i < navButton.length - 1){
-            // 시차이벤트
-        }
-    }
-})
+function scrollMove(i) {
+    const contents = document.querySelectorAll('.container section');
+    window.scrollTo(0, contents[i].offsetTop)
+}
 
 function scrollToNext() {
-    const navButton = document.querySelectorAll('nav ul li')
+    const navButton = document.querySelectorAll('.main-navigator ul li')
     const contents = document.querySelectorAll('.container section');
     for (let i = 0; i < navButton.length; i++) {
         if(navButton[i].classList.contains('active') && i < navButton.length - 1){
@@ -31,3 +26,16 @@ function scrollToNext() {
         }
     }
 }
+
+document.addEventListener('scroll', function(){
+    const navButton = document.querySelectorAll('.main-navigator ul li')
+    const contents = document.querySelectorAll('.container section')
+    const scroll = window.pageYOffset
+    for (let i = 0; i < navButton.length; i++) {
+        const scrollAmount = scroll - contents[i].offsetTop // 스크롤 양 (px)
+        const contentHeight = contents[i].offsetHeight / 200 // 한번 스크롤 될때 배경 감도 (숫자 높을수록 체감 많이됨)
+        const num = scrollAmount / contentHeight // 스크롤된 양 (%)
+        const percent = 90 - num // 50은 포지션값 (60으로 변경되면 바꿔야함)
+        contents[i].setAttribute('style', `background-position: 50% ${percent}%;`)
+    }
+})
