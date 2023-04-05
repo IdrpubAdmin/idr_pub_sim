4e489349babefcdf58ea93b29ee659b92d6b1721
seop277 <97004955+seop2777@users.noreply.github.com>
2023-04-05 18:00:57 +0900

    toast grid test

---
+++ study/toast-grid/app.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <Test></Test>
+    <grid :data="gridProps.data" :columns="gridProps.columns" />
+  </div>
+</template>
+
+<script>
+import Test from '~/test.vue'
+
+import 'tui-grid/dist/tui-grid.css';
+import { Grid } from 'vue3-tui-grid';
+
+export default {
+  components: {
+    Test: Test,
+    grid: Grid
+  },
+  created() {
+    this.gridProps = {
+      data: [
+        // for rowData prop
+        {
+          name: 'Beautiful Lies',
+          artist: 'Birdy'
+        },
+        {
+          name: 'X',
+          artist: 'Ed Sheeran'
+        }
+      ],
+      columns: [
+        // for columnData prop
+        {
+          header: 'Name',
+          name: 'name'
+        },
+        {
+          header: 'Artist',
+          name: 'artist'
+        }
+      ]
+    };
+  }
+}
+</script>
